5247ba0ded429ebc6c3570847b4de768381fa385
arie.haryadi <alternativesong163@yahoo.com>
2019-06-14 10:20:22 +0700

    Deploying

--- src/app/admin/layouts/navbar/navbar.component.ts
+++ src/app/admin/layouts/navbar/navbar.component.ts
@@ -34,6 +34,20 @@ export class NavbarComponent implements OnInit {
        }
    });
   }
+
+  collapse(){
+    this.isCollapsed = !this.isCollapsed;
+    const navbar = document.getElementsByTagName('nav')[0];
+    console.log(navbar);
+    if (!this.isCollapsed) {
+      navbar.classList.remove('navbar-transparent');
+      navbar.classList.add('bg-white');
+    }else{
+      navbar.classList.add('navbar-transparent');
+      navbar.classList.remove('bg-white');
+    }
+
+  }  
   
   sidebarOpen() {
     const toggleButton = this.toggleButton;
