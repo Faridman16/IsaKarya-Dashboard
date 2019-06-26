7a9c3485aff786ae77463434ae4f22577fc95c08
Faridman16 <alternativesong163@yahoo.com>
2019-06-27 06:13:55 +0700

    Update operation module

--- src/app/_services/operational.service.ts
+++ src/app/_services/operational.service.ts
@@ -2,21 +2,24 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { OperationalModel } from '../_models/operationalModel';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OperationalService {
 
-  constructor(private fireDB: AngularFireDatabase) { }
+  constructor(private fireDB: AngularFireDatabase, private http: HttpClient) { }
 
   getOperationalList(): Observable<OperationalModel[]>{
     return this.fireDB.list<OperationalModel>('operational').valueChanges();
   }
 
   addOperational(operatioanl: OperationalModel){
-    const fireOperational = this.fireDB.database.ref('operational');
-    fireOperational.push(operatioanl);
-    console.log(operatioanl +' was pushed');
+    // const fireOperational = this.fireDB.database.ref('operational');
+    // fireOperational.push(operatioanl);
+    // console.log(operatioanl +' was pushed');
+
+    // this.http.post('localhost:4300')
   }
 }
