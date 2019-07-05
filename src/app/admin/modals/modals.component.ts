5f69519fbafbe7a54f18aa62b6460fdf9555a288
Faridman16 <alternativesong163@yahoo.com>
2019-07-06 01:12:24 +0700

    income/outcome

--- src/app/admin/modals/modals.component.ts
+++ src/app/admin/modals/modals.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-
 import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, Form, FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -10,13 +11,46 @@ import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalsComponent implements OnInit {
 
+  trx: FormGroup;
+  nominal:string;
+
   @Input() title = `Information`;
 
   constructor(
-    public activeModal: NgbActiveModal
-  ) {}
+    public activeModal: NgbActiveModal,
+    private fb: FormBuilder,
+    private datePipe: DatePipe,    
+  ) {this.createForm();}
+
+  createForm() {
+    const dateNow = this.datePipe.transform(Date.now(), 'yyyy-MM-dd');
+    this.trx = new FormGroup({
+      type: new FormControl('', [Validators.required]),
+      tanggal: new FormControl(dateNow, [Validators.required]),
+      kategori: new FormControl('', [Validators.required]),
+      jumlah: new FormControl('', [Validators.required]),
+      keterangan: new FormControl('', [Validators.required]),
+    });
+  }  
 
   ngOnInit() {
+  }
+
+  currencyFormat(event) {
+    // const curr:number = this.trx.value.jumlah
+    // this.trx.setValue(this.trx.getRawValue);
+    console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(this.trx.value.jumlah));
+    this.nominal = this.trx.value.jumlah;
+    console.log(this.nominal);
+    // this.trx.valueChanges() = curr;
+    // this.trx.patchValue({
+    //   jumlah: curr, 
+    //   // formControlName2: myValue2 (can be omitted)
+    // });
+  }
+
+  formater(event){
+    console.log(event);
   }
 
 }
